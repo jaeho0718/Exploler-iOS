49c28822eb082b77f41aa9d06e41973662717ea8
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2024-05-17 11:06:44 +0900

    Initial Commit

---
+++ Exploler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Exploler
+//
+//  Created by Lee Jaeho on 5/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Exploler/ExplolerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExplolerApp.swift
+//  Exploler
+//
+//  Created by Lee Jaeho on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ExplolerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
