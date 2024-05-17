a1c525fdf07746d7a23ee73aace6a123b4f99a68
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2024-05-17 16:59:20 +0900

    ✨ 색상 컴포넌트 추가
    다음 색상들을 추가했습니다.
    - Button: Cancel, Save
    - Chip: Plant, Special, Unselected
    - Font: Overlay, SubTitle, Title
    - Theme: OnSurface, Surface

---
+++ Exploler/Component/Color/FontColor.swift
@@ -0,0 +1,17 @@
+//
+//  FontColor.swift
+//  Exploler
+//
+//  Created by Lee Jaeho on 5/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    enum Font {
+        static let overlay = Color("Overlay")
+        static let title = Color("Title")
+        static let subtitle = Color("SubTitle")
+    }
+}
---
+++ Exploler/Component/Color/ThemeColor.swift
@@ -0,0 +1,16 @@
+//
+//  ThemeColor.swift
+//  Exploler
+//
+//  Created by Lee Jaeho on 5/17/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    enum Theme {
+        static let onSurface = Color("OnSurface")
+        static let surface = Color("Surface")
+    }
+}
