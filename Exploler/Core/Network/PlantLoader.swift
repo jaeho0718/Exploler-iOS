dca2577c2434b2e787ce94344f29ba0fd225ec76
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2024-05-19 01:02:21 +0900

    ✨ PlantLoader 추가

---
+++ Exploler/Core/Network/PlantLoader.swift
@@ -0,0 +1,35 @@
+//
+//  PlantLoader.swift
+//  Exploler
+//
+//  Created by Lee Jaeho on 5/19/24.
+//
+
+import Foundation
+
+class PlantLoader {
+    private func getURLComponents(path: String) -> URLComponents {
+        var urlComponent = URLComponents()
+        urlComponent.scheme = "http"
+        urlComponent.host = "43.201.107.162"
+        urlComponent.port = 8080
+        
+        return urlComponent
+    }
+    
+    func fetchAllPlants() async throws -> [PlantModel] {
+        let urlComponent = getURLComponents(path: "/plant")
+        let url = urlComponent.url!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "content-type")
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let models = try JSONDecoder().decode([PlantModel].self, from: data)
+        return models
+    }
+    /*
+    func fetchPlantByID(plantID: String) async throws -> PlantModel {
+        
+    }
+     */
+}
--- ExplolerTests/NetworkTests.swift
+++ ExplolerTests/NetworkTests.swift
@@ -10,23 +10,14 @@ import XCTest
 @testable import Exploler
 
 final class NetworkTests: XCTestCase {
-    var urlComponent: URLComponents!
+    var loader: PlantLoader!
     
     override func setUpWithError() throws {
-        urlComponent = URLComponents()
-        urlComponent.scheme = "http"
-        urlComponent.host = "43.201.107.162"
-        urlComponent.port = 8080
+        loader = PlantLoader()
     }
     
     func testFetchAllPlants() async throws {
-        urlComponent.path = "/plant"
-        let url = urlComponent.url!
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        request.setValue("application/json", forHTTPHeaderField: "content-type")
-        let (data, _) = try await URLSession.shared.data(for: request)
-        let models = try JSONDecoder().decode([PlantModel].self, from: data)
+        let models = try await loader.fetchAllPlants()
         print(models)
         XCTAssert(models.count > 0)
     }
